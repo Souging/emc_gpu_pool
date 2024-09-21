234ea1bc1d0ed6347ba6fe21dfc43162c70ab572
Souging <150511278+Souging@users.noreply.github.com>
2024-09-21 18:48:34 +0800

    Update main.cu

--- src/main.cu
+++ src/main.cu
@@ -6,28 +6,21 @@
 #include <getopt.h>
 #include <string.h>
 #include "sha3.cuh"
-
 #define BLOCKS 128
 #define THREADS 256
 #define N 100000000000  
-
-
-__device__ int lock = 0;  // 锁变量
-
+__device__ int lock = 0;  
 __device__ void lock_acquire() {
     while (atomicCAS(&lock, 0, 1) != 0) {
-        // 等待锁被释放
         __threadfence();  
     }
 }
-
 __device__ void lock_release() {
-    atomicExch(&lock, 0);  // 释放锁
+    atomicExch(&lock, 0);  
 }
-
 // Fixed constants (Host side only)
-__constant__ uint8_t DIFFICULTY[4] = {0x22, 0x33, 0x99, 0x99};
-__constant__ uint8_t CURRENT_CHALLENGE[32] = {0x78, 0x92, 0x55, 0x68, 0x00, 0x99, 0x00, 0x90,
+__constant__ uint8_t DIFFICULTY[4] = {0x22, 0x33, 0x99,0x99};
+__constant__ uint8_t CURRENT_CHALLENGE[32] = {0x78, 0x90, 0x55, 0x58, 0x00, 0x90, 0x00, 0x90,
                                               0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                               0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                               0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
@@ -50,10 +43,10 @@ __device__ void byte_to_hex(uint8_t byte, char* output) {
 
 __global__ void calculate() {
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    __shared__ uint8_t shared_difficulty[4];
+    __shared__ uint8_t shared_difficulty[3];
     __shared__ uint8_t shared_challenge[32];
     __shared__ uint8_t shared_miner_address[20];
-    if (threadIdx.x < 4) shared_difficulty[threadIdx.x] = DIFFICULTY[threadIdx.x];
+    if (threadIdx.x < 3) shared_difficulty[threadIdx.x] = DIFFICULTY[threadIdx.x];
     if (threadIdx.x < 32) shared_challenge[threadIdx.x] = CURRENT_CHALLENGE[threadIdx.x];
     if (threadIdx.x < 20) shared_miner_address[threadIdx.x] = MINER_ADDRESS[threadIdx.x];
     __syncthreads();
@@ -64,10 +57,6 @@ __global__ void calculate() {
     //uint8_t local_miner_address[20];
     //memcpy(local_challenge, shared_challenge, 32);
     //memcpy(local_miner_address, shared_miner_address, 20);
-
-
-
-
     uint64_t block = (uint64_t)(curand_uniform_double(&state) * UINT64_MAX);
     uint8_t data[96];
     uint8_t hash[32];
@@ -80,7 +69,7 @@ __global__ void calculate() {
         for (int j = 0; j < 32; j++) {
             //blockBytes[j] = curand(&state) & 0xFF;
             ((uint32_t*)blockBytes)[j] = curand(&state);
-            ((uint32_t*)blockBytes)[j] ^= (curand(&state) << 16);
+            //((uint32_t*)blockBytes)[j] ^= (curand(&state) << 16);
             //blockBytes[j] ^= (curand(&state) << 16);
 
         }
@@ -90,15 +79,28 @@ __global__ void calculate() {
             printf("bad params\n");
             return;
         }
-        if (*(uint32_t*)hash == *(uint32_t*)shared_difficulty) {
+        bool match = true;
+
+        for(int j = 0; j<3; j++){
+            if(hash[j] != shared_difficulty[j]){
+                match = false;
+                break;
+            }
+        }
+        if (match) {
             lock_acquire();
+            //char result1[65];  // 32 bytes * 2 chars per byte + 1 null terminator
+            //for (int j = 0; j < 32; j++) {
+            //    byte_to_hex(hash[j], &result1[j*2]);
+            //}
+            //result1[64] = '\0'; 
+            //printf("thread => %d hash :0x%s\n", tid, result1);
+            
             char result[65];  // 32 bytes * 2 chars per byte + 1 null terminator
             for (int j = 0; j < 32; j++) {
                 byte_to_hex(blockBytes[j], &result[j*2]);
             }
-            result[64] = '\0';  // 确保字符串正确终止
-            
-            // 一次性打印结果
+            result[64] = '\0'; 
             printf("thread => %d randomValue :0x%s\n", tid, result);
             lock_release();
         }
@@ -108,6 +110,7 @@ __global__ void calculate() {
 int main(int argc, char *argv[]) {
 	int opt;
 	int poolIndex = -1; 
+    //0x32b2C3eE46A65665282DbD50328AFA4679eC1786
 	uint8_t MINER_ADDRESS1[20] = {0x32, 0xb2, 0xc3, 0xee, 0x46, 0xa6, 0x56, 0x65,0x28, 0x2d, 0xbd, 0x50, 0x32, 0x8a, 0xfa, 0x46,0x79, 0xec, 0x17, 0x86};
 	uint8_t MINER_ADDRESS2[20] = {0x9f, 0x07, 0xd1, 0x4c, 0x88, 0xeb, 0x4f, 0x11, 0x1c, 0x9f, 0x9c, 0xc7, 0x66, 0x39, 0xb7, 0xca, 0xeb, 0x62, 0x2b, 0x4b};
 	uint8_t MINER_ADDRESS3[20] = {0xd3, 0x79, 0xed, 0x77, 0x39, 0x39, 0x67, 0x58, 0x7e, 0x54, 0xf5, 0x0a, 0x4e, 0xf6, 0x28, 0xda, 0x08, 0x41, 0x7c, 0xb5};
@@ -133,8 +136,8 @@ int main(int argc, char *argv[]) {
         case 3: selectedMinerAddress = MINER_ADDRESS3; break;
         case 4: selectedMinerAddress = MINER_ADDRESS4; break;
     }
-    cudaMemcpyToSymbol(DIFFICULTY, "\x22\x33\x99\x99", 4);
-    cudaMemcpyToSymbol(CURRENT_CHALLENGE, "\x78\x92\x55\x68\x00\x99\x00\x90\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00", 32);
+    cudaMemcpyToSymbol(DIFFICULTY, "\x12\x34\x88", 3);
+    cudaMemcpyToSymbol(CURRENT_CHALLENGE, "\x78\x90\x55\x58\x00\x90\x00\x90\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00", 32);
     cudaMemcpyToSymbol(MINER_ADDRESS, selectedMinerAddress, 20);
     calculate<<<BLOCKS, THREADS>>>();
     cudaDeviceSynchronize();
